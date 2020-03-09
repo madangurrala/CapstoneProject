283506071dc318dc5094b16e83b27dd67d3f42da
Madan <madangurrala@gmail.com>
2020-03-08 21:42:03 -0400

    Fixed Floating Button

--- app/src/main/java/conestoga/technocrats/capstone/conestogatechnocratscapstoneproj/model/local/da/PropertyDA.java
+++ app/src/main/java/conestoga/technocrats/capstone/conestogatechnocratscapstoneproj/model/local/da/PropertyDA.java
@@ -3,6 +3,7 @@ package conestoga.technocrats.capstone.conestogatechnocratscapstoneproj.model.lo
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
+import androidx.room.Entity;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
